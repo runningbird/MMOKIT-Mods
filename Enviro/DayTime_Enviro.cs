d20a9ee4d3b902a54114e68110ea668f71beace6
Jason Burch <jburch@runningbirdstudios.com>
2022-07-28 15:29:35 -0500

    adding mmo kit scripts to repo

---
+++ Enviro/DayTime_Enviro.cs
@@ -0,0 +1,19 @@
+﻿using MultiplayerARPG;
+using System.Collections;
+using UnityEngine;
+
+namespace Runningbird.Scripts
+{
+    public class DayTime_Enviro : MonoBehaviour
+    {
+        private void Update()
+        {
+            // Update time of day percent while network active only
+            if (Application.isPlaying && BaseGameNetworkManager.Singleton.IsNetworkActive)
+            {
+                EnviroSkyMgr.instance.SetTimeOfDay(GameInstance.Singleton.DayNightTimeUpdater.TimeOfDay);
+            }
+
+        }
+    }
+}
