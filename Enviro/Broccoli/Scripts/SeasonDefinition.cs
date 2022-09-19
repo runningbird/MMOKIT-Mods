0afb574697a2c6a5673ea85eb04752c03d1c3d03
Jason Burch <jburch@runningbirdstudios.com>
2022-09-19 17:12:36 -0500

    Initial Commit of the SeasonManager for Broccoli Trees and Enviro
    This Addon allows you to change the SproutKeys on a Broccoli tree when the Season changes in the EnviroManager Instance.

---
+++ Enviro/Broccoli/Scripts/SeasonDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.MMO.Scripts.Broccoli
+{
+    [CreateAssetMenu(fileName = "BroccoliEnviroSeasonDefinition", menuName = "RunningbirdStudios/Enviro/Brocolli/SeasonDefinition", order = 1)]
+    public class SeasonDefinition : ScriptableObject
+    {
+        public EnviroSeasons.Seasons EnviroSeason;
+        public string SproutMapperKeyName;
+
+    }
+}
---
+++ Enviro/Broccoli/Scripts/SeasonsManager.cs
@@ -0,0 +1,103 @@
+﻿using Broccoli.Factory;
+using Broccoli.Pipe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.MMO.Scripts.Broccoli
+{
+    public class SeasonsManager : MonoBehaviour
+    {
+        private TreeFactory treeFactory;
+        private Pipeline pipeline;
+        public EnviroSeasons.Seasons CurrentSeason;
+        public SeasonDefinition[] seasonDefinitions;
+        public IEnumerable<SeasonDefinition> currentSeasonDefinition;
+
+        //private string[] sproutMapperKeyNames = new string[] { "Autumn", "Green" };
+        private List<string> sproutMapperKeyNames;
+        private int sproutMapperSelected = 0;
+
+        private void Awake()
+        {
+            treeFactory = GetComponent<TreeFactory>();
+            pipeline = treeFactory.localPipeline;
+        }
+
+        private void Start()
+        {
+            CurrentSeason = EnviroSkyMgr.instance.GetCurrentSeason();
+            SetSeason(CurrentSeason);
+
+            EnviroSkyMgr.instance.OnSeasonChanged += (EnviroSeasons.Seasons season) =>
+            {
+                SetSeason(season);
+                Debug.Log("Season changed");
+            };
+
+            
+        }
+
+
+        public void SetSeason(EnviroSeasons.Seasons season)
+        {
+            if (treeFactory)
+            {
+                if (pipeline != null && pipeline.IsValid())
+                {
+                    switch (season)
+                    {
+                        case EnviroSeasons.Seasons.Spring:
+                            {
+                                break;
+                            }
+                        case EnviroSeasons.Seasons.Summer:
+                            {
+                                break;
+                            }
+                        case EnviroSeasons.Seasons.Autumn:
+                            {
+                                break;
+                            }
+                        case EnviroSeasons.Seasons.Winter:
+                            {
+                                break;
+                            }
+                    }
+                    SetSproutMapperKeyNames(season);
+                    treeFactory.ProcessPipelinePreview(null, true);
+                    CurrentSeason = EnviroSkyMgr.instance.GetCurrentSeason();
+                }
+            }
+        }
+
+        private void SetSproutMapperKeyNames(EnviroSeasons.Seasons newSeason)
+        {
+            //Get The New Season
+            IEnumerable<SeasonDefinition> updatedSeason = from SeasonDefinition s in seasonDefinitions
+                                                          where s.EnviroSeason == newSeason
+                                                          select s;
+
+            currentSeasonDefinition = from SeasonDefinition season in seasonDefinitions
+                                      where season.EnviroSeason == CurrentSeason
+                                      select season;
+
+            if (updatedSeason.First().SproutMapperKeyName != currentSeasonDefinition.First().SproutMapperKeyName)
+            {
+                //Replace the CurrentSeasonDefinition with the updated one.
+                pipeline.ReplaceElements(currentSeasonDefinition.First().SproutMapperKeyName, updatedSeason.First().SproutMapperKeyName);
+            }
+            else
+            {
+                //get different SproutMapperKeyName
+                IEnumerable<SeasonDefinition> differentSeason = from SeasonDefinition s in seasonDefinitions
+                                                                where s.SproutMapperKeyName != updatedSeason.First().SproutMapperKeyName
+                                                                select s;
+                pipeline.ReplaceElements(differentSeason.First().SproutMapperKeyName, updatedSeason.First().SproutMapperKeyName);
+            }
+            //Set the CurrentSeadonDefinition to the updated
+            currentSeasonDefinition = updatedSeason;
+        }
+    }
+}
